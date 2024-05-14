315963cdc2a6f4d994b229540b7829d0d2fa30de
Sh4yn3 <164798627+Sh4yn3@users.noreply.github.com>
2024-05-14 12:16:36 +1200

    git commit

---
+++ routes.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template
+import sqlite3
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/artstyle')
+def artstylenav():
+    return render_template('artstylenav.html')
+
+
+@app.route('/mediums')
+def mediumsnav():
+    return render_template('mediumsnav.html')
+
+
+@app.route('/artstyle/<int:id>')
+def artstyles(id):
+    conn = sqlite3.connect('ARTGUIDE.db')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM ArtStyles WHERE id=?;", (id,))
+    artstyles = cur.fetchone()
+    conn.close()
+    return render_template("artstyle.html", artstyles=artstyles)
+
+
+@app.route('/medium/<int:id>')
+def medium(id):
+    conn = sqlite3.connect('ARTGUIDE.db')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM Mediums WHERE id=?", (id,))
+    medium = cur.fetchone()
+    conn.close()
+    return render_template("medium.html", medium=medium)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
