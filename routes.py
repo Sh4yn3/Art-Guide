b379d6f1dcb763342e71abd06d242e2fc6c00e76
Sh4yn3 <164798627+Sh4yn3@users.noreply.github.com>
2024-06-18 21:57:41 +1200

    commit

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sqlite3
 
 
@@ -10,12 +10,12 @@ def home():
     return render_template('home.html')
 
 
-@app.route('/artstyles')
+@app.route('/artstyle')
 def artstylenav():
     return render_template('artstylenav.html')
 
 
-@app.route('/mediums')
+@app.route('/medium')
 def mediumsnav():
     return render_template('mediumsnav.html')
 
@@ -54,6 +54,22 @@ def triangles(size):
         return "Size must be a positive integer.", 400
     triangle = generate_triangle(size)
     return f"<pre>{triangle}</pre>"
+
+
+# For the search function
+
+@app.route('/')
+def index():
+    return render_template('search.html')
+
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        search = request.form.get('searchterm')
+        return f'You asked to search for \"{search}\"'
+    else:
+        return "poop"
 
 
 if __name__ == '__main__':
