d430cc512639ad3c70c642431dc2c05cfe3437ef
Sh4yn3 <164798627+Sh4yn3@users.noreply.github.com>
2024-07-29 21:26:23 +1200

    commit

--- routes.py
+++ routes.py
@@ -26,6 +26,11 @@ def mediumsnav():
     return render_template('mediumsnav.html')
 
 
+@app.route('/artist')
+def artistnav():
+    return render_template('artistnav.html')
+
+
 @app.route('/artstyle/<int:id>')
 def artstyle(id):
     conn = get_db()
@@ -40,10 +45,20 @@ def artstyle(id):
 def medium(id):
     conn = get_db()
     cur = conn.cursor()
-    cur.execute('SELECT * WHERE id = ?', (id,))
+    cur.execute('SELECT * WHERE Mediums WHERE id = ?', (id,))
     medium = cur.fetchone()
     conn.close()
     return render_template('medium.html', medium=medium)
+
+
+@app.route('/artist/<int:id>')
+def artist(id):
+    conn = get_db()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM Artists WHERE id = ?", (id,))
+    artist = cur.fetchone()
+    conn.close()
+    return render_template('artist.html', artist=artist)
 
 
 # For a working Search Function
@@ -58,15 +73,30 @@ def search():
 def query_database(search_term):
     conn = get_db()
     cursor = conn.cursor()
-    cursor.execute('''
-                   SELECT 'ArtStyle' AS type, id, name, description
-                   FROM ArtStyles WHERE name LIKE ? UNION
-                   SELECT 'Medium' AS type, id, name,
-                   description FROM Mediums WHERE name LIKE ?
-                   ''', ('%' + search_term + '%', '%' + search_term + '%'))
+    cursor.execute(
+        '''
+        SELECT 'ArtStyle' AS type, id, name, description
+        FROM ArtStyles  WHERE name LIKE ?
+        UNION
+        SELECT 'Medium' AS type, id, name, description
+        FROM Mediums WHERE name LIKE ?
+        UNION
+        SELECT 'Artist' AS type, id, name, description
+        FROM Artists
+        WHERE name LIKE ?
+        ''', ('%' + search_term + '%',
+              '%' + search_term + '%',
+              '%' + search_term + '%'))
     results = cursor.fetchall()
     conn.close()
     return results
+
+
+# Test
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
 
 
 if __name__ == '__main__':
