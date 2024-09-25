40c6c9bbd44f8418eddc1fbbfc4e69e0ff16dbbe
Sh4yn3 <164798627+Sh4yn3@users.noreply.github.com>
2024-09-26 07:33:00 +1200

    commit

--- routes.py
+++ routes.py
@@ -1,160 +1,123 @@
 from flask import Flask, request, render_template
 import sqlite3
 
+
 app = Flask(__name__)
 DATABASE = 'ARTGUIDE.db'
 
 
-# DATABASE
+# Efficient Database Query Function
+def db_query(query, multiple=True):
+    with sqlite3.connect(DATABASE) as conn:
+        cursor = conn.cursor()
+        cursor.execute(query)
+        if multiple:
+            return cursor.fetchall()
+        return cursor.fetchone()
 
-def get_db():
-    conn = sqlite3.connect(DATABASE)
-    return conn
 
-
+# Query Database (search specific)
 def query_database(search_term):
-    conn = get_db()
-    cursor = conn.cursor()
-
-    # Query for ArtStyles
-    cursor.execute("""
-        SELECT *
-        FROM ArtStyles
-        WHERE name LIKE ?
-    """, ('%' + search_term + '%',))
-    artstyles = cursor.fetchall()
-
-    # Query for Mediums
-    cursor.execute("""
-        SELECT *
-        FROM Mediums
-        WHERE name LIKE ?
-    """, ('%' + search_term + '%',))
-    mediums = cursor.fetchall()
-
-    # Query for Artists
-    cursor.execute("""
-        SELECT *
-        FROM Artists
-        WHERE name LIKE ?
-    """, ('%' + search_term + '%',))
-    artists = cursor.fetchall()
-
-    conn.close()
+    search_term = f"'%{search_term}%'"
     return {
-        'artstyles': artstyles,
-        'mediums': mediums,
-        'artists': artists
+        'artstyles': db_query(f'''
+                              SELECT * FROM ArtStyles
+                              WHERE name LIKE {search_term}
+                              '''),
+        'mediums': db_query(f'''
+                            SELECT * FROM Mediums
+                            WHERE name LIKE {search_term}
+                            '''),
+        'artists': db_query(f'''
+                            SELECT * FROM Artists
+                            WHERE name LIKE {search_term}
+                            ''')
     }
 
 
-# WEBPAGES
-
-@app.route('/test')
-def dontmind():
-    return render_template('dontmind.html')
-
-
+# ROUTES FOR WEBPAGES
 @app.route('/')
 def home():
-    latest_mediums = get_latest_mediums(limit=4)
-    latest_artstyles = get_latest_artstyles(limit=4)
-    latest_artists = get_latest_artists(limit=4)
-    return render_template('home.html', latest_mediums=latest_mediums,
-                           latest_artstyles=latest_artstyles,
-                           latest_artists=latest_artists)
+    return render_template(
+        'home.html',
+        # Query to showcase newly added content
+        latest_mediums=db_query('''
+                                SELECT * FROM Mediums
+                                ORDER BY created_at DESC LIMIT 4
+                                '''),
+        latest_artstyles=db_query('''
+                                  SELECT * FROM ArtStyles
+                                  ORDER BY created_at DESC LIMIT 4
+                                  '''),
+        latest_artists=db_query('''
+                                SELECT * FROM Artists
+                                ORDER BY created_at DESC LIMIT 4
+                                ''')
+    )
 
 
+# Showcases navigation of artstyles
 @app.route('/artstyle')
 def artstylenav():
-    conn = get_db()
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM ArtStyles")
-    artstyles = cur.fetchall()
-    conn.close()
-    return render_template('artstylenav.html', artstyles=artstyles)
+    return render_template('artstylenav.html',
+                           artstyles=db_query("SELECT * FROM ArtStyles"))
 
 
 @app.route('/medium')
 def mediumsnav():
-    conn = get_db()
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM Mediums")
-    mediums = cur.fetchall()
-    conn.close()
-    return render_template('mediumsnav.html', mediums=mediums)
+    return render_template('mediumsnav.html',
+                           mediums=db_query("SELECT * FROM Mediums"))
 
 
 @app.route('/artist')
 def artistnav():
-    conn = get_db()
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM Artists")
-    artists = cur.fetchall()
-    conn.close()
-    return render_template('artistnav.html', artists=artists)
+    return render_template('artistnav.html',
+                           artists=db_query("SELECT * FROM Artists"))
 
 
 @app.route('/artstyle/<int:id>')
 def artstyle(id):
-    conn = get_db()
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM ArtStyles WHERE id = ?", (id,))
-    artstyle = cur.fetchone()
+    artstyle = db_query(f"SELECT * FROM ArtStyles WHERE id={id}",
+                        multiple=False)
     if not artstyle:
         return render_template('error.html', message="ArtStyle not found"), 404
-    cur.execute("SELECT * FROM Artworks WHERE artstyle_id = ? ORDER BY year",
-                (id,))
-    artworks = cur.fetchall()
-    conn.close()
+    artworks = db_query(f'''
+                        SELECT * FROM Artworks
+                        WHERE artstyle_id={id} ORDER BY year
+                        ''')
     return render_template('artstyle.html', artstyle=artstyle,
                            artworks=artworks)
 
 
 @app.route('/medium/<int:id>')
 def medium(id):
-    conn = get_db()
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM Mediums WHERE id = ?", (id,))
-    medium = cur.fetchone()
-
-    # if no valid form is found, leads to 404 page
+    medium = db_query(f"SELECT * FROM Mediums WHERE id={id}", multiple=False)
     if not medium:
         return render_template('error.html', message="Medium not found"), 404
-    cur.execute("SELECT * FROM Artworks WHERE medium_id = ? ORDER BY year",
-                (id,))
-    artworks = cur.fetchall()
-    conn.close()
-    return render_template('medium.html', medium=medium,
-                           artworks=artworks, )
+    artworks = db_query(f'''
+                        SELECT * FROM Artworks
+                        WHERE medium_id={id} ORDER BY year
+                        ''')
+    return render_template('medium.html', medium=medium, artworks=artworks)
 
 
 @app.route('/artist/<int:id>')
 def artist(id):
-    conn = get_db()
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM Artists WHERE id = ?", (id,))
-    artist = cur.fetchone()
+    artist = db_query(f"SELECT * FROM Artists WHERE id={id}", multiple=False)
     if not artist:
         return render_template('error.html', message="Artist not found"), 404
-    cur.execute("SELECT * FROM Artworks WHERE artist_id = ? ORDER BY year",
-                (id,))
-    artworks = cur.fetchall()
-    conn.close()
-    return render_template('artist.html', artist=artist,
-                           artworks=artworks)
+    artworks = db_query(f'''
+                        SELECT * FROM Artworks
+                        WHERE artist_id={id} ORDER BY year
+                        ''')
+    return render_template('artist.html', artist=artist, artworks=artworks)
 
-
-# FUNCTIONS
 
 @app.route('/search', methods=['POST'])
 def search():
     search_term = request.form['search_term']
     results = query_database(search_term)
-
-    # Check if no results were found
     no_results = all(len(result) == 0 for result in results.values())
-
     return render_template('search.html', results=results,
                            no_results=no_results)
 
@@ -162,48 +125,6 @@ def search():
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('error.html'), 404
-
-
-def get_latest_artists(limit=5):
-    conn = get_db()
-    cur = conn.cursor()
-    cur.execute("""
-        SELECT *
-        FROM Artists
-        ORDER BY created_at DESC
-        LIMIT ?
-    """, (limit,))
-    latest_artists = cur.fetchall()
-    conn.close()
-    return latest_artists
-
-
-def get_latest_artstyles(limit=5):
-    conn = get_db()
-    cur = conn.cursor()
-    cur.execute("""
-        SELECT *
-        FROM ArtStyles
-        ORDER BY created_at DESC
-        LIMIT ?
-    """, (limit,))
-    latest_artstyles = cur.fetchall()
-    conn.close()
-    return latest_artstyles
-
-
-def get_latest_mediums(limit=5):
-    conn = get_db()
-    cur = conn.cursor()
-    cur.execute("""
-        SELECT *
-        FROM Mediums
-        ORDER BY created_at DESC
-        LIMIT ?
-    """, (limit,))
-    latest_mediums = cur.fetchall()
-    conn.close()
-    return latest_mediums
 
 
 if __name__ == '__main__':
